065cee474ec8096fd2497e2293edd9d3483b4b31
ABCDE123sehfka1 <105478730+ABCDE123sehfka1@users.noreply.github.com>
2022-05-13 14:17:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@ echo    'Removing chromium-browser'
 sudo apt remove chromium-browser
 sudo rm -rf /usr/local/share/chromium
 echo    'Install Google_chrome && chrome driver'
-sudo apt install ./assets/chrome.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install google-chrome-stable_current_amd64.deb
+wget https://registry.npmmirror.com/-/binary/chromedriver/101.0.4951.41/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo mv -f chromedriver /usr/local/share/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
 echo    'Install Dependiences'
 python -m pip install selenium
